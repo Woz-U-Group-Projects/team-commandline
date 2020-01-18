992c52c8777870282c6362a187cb6e5a2541fd52
WolffJosephN <wolff.joseph.n@gmail.com>
2020-01-18 00:28:49 -0600

    Deleted comment

--- client-react/src/components/Project.js
+++ client-react/src/components/Project.js
@@ -16,9 +16,6 @@ class Project extends React.Component {
     // Java Spring Boot uses port 8080
     let url = "http://localhost:8080/tasks";
 
-    // C# dotnetcore uses port 5000
-    //let url = "http://localhost:5000/projects";
-
     // Express uses port 3001 (react uses 3000)
     //let url = "http://localhost:3001/tasks";
     axios.get(url).then(response => this.setState({ projects: response.data }));
